9c4e50027a2873c75a33e8ef4925590c2dc901cd
Aayushi Gupta <78559980+guptaaayushi09@users.noreply.github.com>
2026-08-04 00:32:40 +0530

    Time: 14 ms (24.51%), Space: 44.2 MB (48.92%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -1,74 +1,64 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+
 class Solution {
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-       // target value 
-       // go k distance  -> down -> up ?
-       // convert it into graph -> then start from target -> go bfs till k depth 
-       List<Integer> result = new ArrayList<>();
-       if(root == null) return result;
-       Map<TreeNode, TreeNode> parentMap = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+        if(root == null) return result;
+        Map<TreeNode, TreeNode> parentMap = new HashMap<>();
+        populateParentMap(root,parentMap);
 
-       populateParentMap(root,parentMap);
-
-        Queue<TreeNode> queue = new LinkedList<>();
         Set<TreeNode> visited = new HashSet<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+
         queue.offer(target);
         visited.add(target);
         int currDis = 0;
         while(!queue.isEmpty()){
+            int size = queue.size();
             if(currDis == k){
-                for(TreeNode node : queue){
-                    result.add(node.val);
+                for(int i = 0;i<size;i++){
+                    result.add(queue.poll().val);
                 }
                 return result;
             }
-
-            int size = queue.size();
             for(int i = 0;i<size;i++){
-                TreeNode current = queue.poll();
-                if(current.left != null && !visited.contains(current.left)){
-                    visited.add(current.left);
+               TreeNode current = queue.poll();
+               if(current.left != null && !visited.contains(current.left)){
                     queue.offer(current.left);
-                }
-                if(current.right != null && !visited.contains(current.right)){
-                    visited.add(current.right);
+                    visited.add(current.left);
+               }
+               if(current.right != null && !visited.contains(current.right)){
                     queue.offer(current.right);
-                }
-
-               TreeNode parent = parentMap.get(current);
-               if(parent != null && !visited.contains(parent)){
-                visited.add(parent);
-                queue.offer(parent);
+                    visited.add(current.right);
                }
 
+              TreeNode parent = parentMap.get(current);
+              if(parent != null && !visited.contains(parent)){
+                queue.offer(parent);
+                visited.add(parent);
+              }
             }
-             currDis++;
+            currDis++;
         }
         return result;
-
-    }
-    private void populateParentMap(TreeNode root, Map<TreeNode,TreeNode> parentMap){
+     }
+     private void populateParentMap(TreeNode root, Map<TreeNode, TreeNode> map){
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
-        while(!queue.isEmpty()){
-            TreeNode current = queue.poll();
-            if(current.left != null) {
-                parentMap.put(current.left, current);
-                queue.offer(current.left);
 
+        while(!queue.isEmpty()){
+          int size = queue.size();
+
+          for(int i =0;i<size;i++){
+            TreeNode current = queue.poll();
+            if(current.left != null){
+                queue.offer(current.left);
+                map.put(current.left, current);
             }
             if(current.right != null){
-                parentMap.put(current.right, current);
                 queue.offer(current.right);
+                map.put(current.right, current);
             }
+          }
         }
-    }
+     }
 }
