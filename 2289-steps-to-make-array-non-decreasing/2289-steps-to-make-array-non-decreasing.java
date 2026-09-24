5d9f75b3fe1226b3fe58ed5d1fcd64ff4c6656ad
Aayushi Gupta <78559980+guptaaayushi09@users.noreply.github.com>
2026-09-24 23:18:11 +0530

    Time: 63 ms (5.28%), Space: 102 MB (21.55%) - LeetHub

--- 2289-steps-to-make-array-non-decreasing/2289-steps-to-make-array-non-decreasing.java
+++ 2289-steps-to-make-array-non-decreasing/2289-steps-to-make-array-non-decreasing.java
@@ -1,21 +1,19 @@
 class Solution {
     public int totalSteps(int[] nums) {
-        // we have to count the number 
-     //traverse reverse the nums and store value with count
-     // if st.top <= current pop and increment the count of current and keep doing till st.top > current 
-     int n = nums.length;
-     int ans = 0;
-     Stack<Pair<Integer,Integer>> st =  new Stack<>();
-     st.push(new Pair<>(nums[n-1],0));
-     for(int i = n-2;i>=0;i--){
-        int count = 0;
-        while(!st.isEmpty() && nums[i] > st.peek().getKey()){
-            count = Math.max(count+1,st.peek().getValue());
-            st.pop();
-        }
-        ans = Math.max(ans,count);
-        st.push(new Pair(nums[i],count));
-     }
+        // we have to count the number of iteration required to remove the and reach till the point left is < than right or right is greater than left
+        int n = nums.length;
+        int ans = 0;
+        Stack<Pair<Integer,Integer>> st = new Stack<>();
+        st.push(new Pair(nums[n-1],0));
+        for(int i = n-2;i>=0;i--){
+            int count = 0; // for every index it should be new count
+            while(!st.isEmpty() && nums[i] > st.peek().getKey()){
+                count = Math.max(count +1, st.peek().getValue());
+                st.pop();
+            }
+            ans = Math.max(ans,count);
+             st.push(new Pair(nums[i],count));
+         }
      return ans;
     }
 }
