3e4a8e510520b939d03a010341f1fcd3312242e9
Aayushi Gupta <78559980+guptaaayushi09@users.noreply.github.com>
2026-09-25 18:26:45 +0530

    Time: 20 ms (45%), Space: 53.8 MB (74.58%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -3,18 +3,20 @@ class Solution {
         int m = s.length();
         int n = t.length();
 
-        int[] dp = new int[n+1];
-        dp[0] = 1;
-
+        int[][] dp = new int[m+1][n+1];
+        for(int i = 0;i<=m;i++){
+            // dp[0][i] = 0; // s is empty them it can make subsequence and this is before written because dp[0][0] = 1 will overrid ein nexr line by dp[i][0] = 1; not needed as in java by default values are 0
+            dp[i][0] = 1;  // t is empty that can be made by just one empty string
+        }
         for(int i = 1;i<=m;i++){
-            char sChar = s.charAt(i-1);
-            for(int j = n;j>=1;j--){
-                char tChar = t.charAt(j-1);
-                if(sChar == tChar){
-                    dp[j] = dp[j] + dp[j-1];
+            for(int j = 1;j<=n;j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    dp[i][j] = (dp[i-1][j-1] + dp[i-1][j]);// choose and skip both as we can have multiple subsequence
+                }else{
+                    dp[i][j] = dp[i-1][j];//skip
                 }
             }
         }
-        return dp[n];
+        return dp[m][n];
     }
 }
